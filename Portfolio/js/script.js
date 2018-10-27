efea2fecea2a35c1db3ac18d4400797d8119503f
Clinton <clintogechi@gmail.com>
2018-10-27 15:23:39 +0300

    Add files via upload

---
+++ Portfolio/js/script.js
@@ -0,0 +1,16 @@
+/*$(Document).ready(function(){
+    $("button").click(function(){
+        $("img").hide();
+    });
+});
+var hi = "Hey";
+var person = "Stranger";
+//console.log(hi +" " +person)
+//alert(name + " " + age);
+//prompt("What is your name");
+
+function firstFunction(){
+    alert(hi + " " + person)
+};
+
+firstFunction()*/
